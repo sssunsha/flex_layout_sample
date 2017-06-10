91a5ea9ebbb3e60fcaf3e14052ef4d42ebc3f54e
Will Sun <412502331@qq.com>
2017-06-10 21:04:22 +0800

    add block component

--- flexSample/src/app/app.module.ts
+++ flexSample/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { FlexSampleComponent } from './flex-sample/flex-sample.component';
+import { BlockComponent } from './block/block.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FlexSampleComponent
+    FlexSampleComponent,
+    BlockComponent
   ],
   imports: [
     BrowserModule,
