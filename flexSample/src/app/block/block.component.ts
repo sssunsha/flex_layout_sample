695253631f9951ac2eeb36e3657394e1c2fa6a1a
Will Sun <412502331@qq.com>
2017-06-10 23:01:06 +0800

    try to set random color for every block

--- flexSample/src/app/block/block.component.ts
+++ flexSample/src/app/block/block.component.ts
@@ -3,20 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-block',
   templateUrl: './block.component.html',
-  styleUrls: ['./block.component.css']
+  styleUrls: ['./block.component.css'],
+  styles:['background-color: #ff8080;']
 })
 export class BlockComponent implements OnInit {
+
+  color:string;
 
   constructor() { }
 
   ngOnInit() {
 
     // base on the width and height to draw the block
+    this.generateBlockColor();
   }
 
 
   generateBlockColor(){
-    
+    let red = parseInt((255 * Math.random()).toString()).toString(16);
+    let green = parseInt((255 * Math.random()).toString()).toString(16);
+    let blue = parseInt((255 * Math.random()).toString()).toString(16);
+
+    this.color = `#${red}${green}${blue}`; 
+    console.log(this.color);
   }
 
 }
